b6571fc6294b7702564cc999a0f89d464c2b06cf
DeLucca7 <eduardo.lucca4@gmail.com>
2024-08-10 15:56:08 -0300

    Setup monorepo with gateway, orders, service, users and auth service

---
+++ auth-service/src/test/java/microsservice_order_app/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microsservice_order_app.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/microsservice_order_app/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microsservice_order_app.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
