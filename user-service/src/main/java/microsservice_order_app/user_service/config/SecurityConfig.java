2c9e91091a6d73832ca7a11aca54285c5da09faf
DeLucca7 <eduardo.lucca4@gmail.com>
2024-08-21 17:44:06 -0300

    feat: security initial configs

---
+++ user-service/src/main/java/microsservice_order_app/user_service/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package microsservice_order_app.user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll())
+                .formLogin().disable()
+                .csrf().disable()
+                .headers().frameOptions().sameOrigin()
+                .and().build();
+    }
+}
