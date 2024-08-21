4b3cb72b1e4d00198cf73936a3ed844f820dfe92
DeLucca7 <eduardo.lucca4@gmail.com>
2024-08-21 17:43:41 -0300

    build: configure ModelMapper as a bean injectable

---
+++ user-service/src/main/java/microsservice_order_app/user_service/config/BeanConfig.java
@@ -0,0 +1,21 @@
+package microsservice_order_app.user_service.config;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setPropertyCondition(Conditions.isNotNull());
+        return modelMapper;
+    }
+}
