9ba5b14e1b5fa8c83d0901942a913b2ae0de425b
DeLucca7 <eduardo.lucca4@gmail.com>
2024-08-24 00:09:15 -0300

    feat: replace methods to new spring version

--- user-service/src/main/java/microsservice_order_app/user_service/config/security/SecurityConfig.java
+++ user-service/src/main/java/microsservice_order_app/user_service/config/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -24,13 +26,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-                .formLogin().disable()
-                .httpBasic().disable()
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .headers().frameOptions().sameOrigin()
-                .and().build();
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .build();
     }
 
     @Bean
@@ -40,7 +43,8 @@ public class SecurityConfig {
                 "/swagger-ui.html/**",
                 "/swagger-resources/**",
                 "/swagger-ui/**",
-                "/v3/api-docs/**");
+                "/v3/api-docs/**",
+                "/h2-console/**");
     }
 
     @Bean
--- user-service/src/main/java/microsservice_order_app/user_service/config/security/SecurityFilter.java
+++ user-service/src/main/java/microsservice_order_app/user_service/config/security/SecurityFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,7 +27,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     private AuthTokenService authTokenService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = request.getHeader("Authorization");
 
@@ -43,5 +46,6 @@ public class SecurityFilter extends OncePerRequestFilter {
         } catch (Exception e) {
             log.error("M=doFilterInternal Occurred an error, message={}", e.getMessage());
         }
+        filterChain.doFilter(request, response);
     }
 }
